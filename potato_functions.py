31bc18e21c4e161978899604dc2dee867ccaf23a
Fini <enfinity08@gmail.com>
2024-01-10 10:47:56 -0500

    updating with CRUD logic

--- potato_functions.py
+++ potato_functions.py
@@ -1,4 +1,12 @@
-
-def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date):
-    pass
-
+def create_potatoes (owner_name, owner_discord_id, name, potato_type, price, accomplishment, date))
+  #logic for creating
+  pass
+def read_potatoes ()
+  #logic for how to find info here
+  pass
+def update_potatoes ()
+  #logic for how to find info here
+  pass
+def delete_potatoes ()
+  #logic for how to find info here
+  pass
