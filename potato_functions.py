1a074d941d31405b336eae0822ebc4f53a0b5103
Fini <enfinity08@gmail.com>
2024-01-10 11:17:07 -0500

    updating Potatoes to potato

--- potato_functions.py
+++ potato_functions.py
@@ -1,12 +1,15 @@
-def create_potatoes (owner_name, owner_discord_id, name, potato_type, price, accomplishment, date):
-  #logic for creating
-  pass
-def read_potatoes ():
-  #logic for how to find info here
-  pass
-def update_potatoes ():
-  #logic for how to find info here
-  pass
-def delete_potatoes ():
-  #logic for how to find info here
-  pass
+def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date):
+    # logic for creating
+    pass
+
+def read_potato():
+    # logic for how to find info here
+    pass
+
+def update_potato():
+    # logic for how to find info here
+    pass
+
+def delete_potato():
+    # logic for how to find info here
+    pass
