8ece4d34cfd766c8459a33cd28e01f4d69e6d3a5
raspberry <jamangi.jmg@gmail.com>
2024-01-11 13:06:09 -0600

    split up the create_potato tests

--- test_create_potato.py
+++ test_create_potato.py
@@ -1,8 +1,7 @@
 from potato_functions import create_potato
 
 
-def test_create_potato_demo():
-    # Input values for a demo test
+def test_create_potato_returns_a_dict():
     owner_name = "John Doe"
     owner_discord_id = 123456789
     name = "Demo Achievement"
@@ -11,20 +10,50 @@ def test_create_potato_demo():
     accomplishment = "Demo Accomplishment"
     date = "2024-01-10"
 
-    # Call the create_potato function
     potato = create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date)
 
-    # Check if the output is a dictionary
     assert isinstance(potato, dict)
 
-    # Check if all expected keys are present in the output dictionary
+
+def test_create_potato_has_correct_keys():
+    owner_name = "John Doe"
+    owner_discord_id = 123456789
+    name = "Demo Achievement"
+    potato_type = "Demo Type"
+    price = 8.99
+    accomplishment = "Demo Accomplishment"
+    date = "2024-01-10"
+
+    potato = create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date)
     assert set(potato.keys()) == {'owner_name', 'owner_discord_id', 'name', 'potato_type', 'price', 'accomplishment', 'date'}
 
-    # Check if each key is mapped to the corresponding input value
+
+def test_create_potato_properly_sets_owner_name():
+    owner_name = "John Doe"
+    owner_discord_id = 123456789
+    name = "Demo Achievement"
+    potato_type = "Demo Type"
+    price = 8.99
+    accomplishment = "Demo Accomplishment"
+    date = "2024-01-10"
+
+    potato = create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date)
     assert potato['owner_name'] == owner_name
     assert potato['owner_discord_id'] == owner_discord_id
     assert potato['name'] == name
     assert potato['potato_type'] == potato_type
     assert potato['price'] == price
     assert potato['accomplishment'] == accomplishment
+
+
+def test_create_potato_properly_sets_date():
+    owner_name = "John Doe"
+    owner_discord_id = 123456789
+    name = "Demo Achievement"
+    potato_type = "Demo Type"
+    price = 8.99
+    accomplishment = "Demo Accomplishment"
+    date = "2024-01-10"
+
+    potato = create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date)
     assert potato['date'] == date
