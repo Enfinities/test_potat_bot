56158e65ea3ca0d5d5a90fa320736a2b8f1f782c
Fini <enfinity08@gmail.com>
2024-01-11 14:57:05 -0500

    Test #2 solved!

--- potato_functions.py
+++ potato_functions.py
@@ -1,5 +1,11 @@
 def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date):
-        potato = {owner_name: "John Doe"}
+        potato = {"owner_name": owner_name}
+        potato = {123456789: owner_discord_id}
+        potato = {"name": name}
+        potato = {"potato_type": potato_type}
+        potato = {0.00: price}
+        potato = {"accomplishment": accomplishment}
+        potato = {"date": date}
         return potato
 
 def read_potato():
