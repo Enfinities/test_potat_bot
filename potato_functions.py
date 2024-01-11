a8d71bd49117b125fa22f4c02dc7534d82ba47d3
Fini <enfinity08@gmail.com>
2024-01-11 14:45:26 -0500

    Test #1 solved!

--- potato_functions.py
+++ potato_functions.py
@@ -1,12 +1,13 @@
 def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date):
-    # logic for creating
-    pass
+        potato = {owner_name: "John Doe"}
+        return potato
 
 def read_potato():
     # logic for how to find info here
     pass
 
 def update_potato():
+
     # logic for how to find info here
     pass
 
