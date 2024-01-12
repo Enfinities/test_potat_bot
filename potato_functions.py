7f25f9a82968d755c85ded1d5eeef3b4866092c5
Fini <enfinity08@gmail.com>
2024-01-12 16:24:30 -0500

    add parameters to read function

--- potato_functions.py
+++ potato_functions.py
@@ -2,7 +2,7 @@ def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomp
         potato = {"owner_name": owner_name,"owner_discord_id": owner_discord_id,"name": name,"potato_type": potato_type,"price": price,"accomplishment": accomplishment,"date": date}
         return potato
 
-def read_potatos_by_discord_id():
+def read_potatos_by_discord_id('a','b'):
     # logic for how to find info here
     result_potatoes = []
     for potato in potatoes:
