73f3f65df5da115a5f41cae542e6a5b7efe75e56
Fini <enfinity08@gmail.com>
2024-01-12 16:11:30 -0500

    Test #3&4 solved!

--- potato_functions.py
+++ potato_functions.py
@@ -2,9 +2,14 @@ def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomp
         potato = {"owner_name": owner_name,"owner_discord_id": owner_discord_id,"name": name,"potato_type": potato_type,"price": price,"accomplishment": accomplishment,"date": date}
         return potato
 
-def read_potato():
+def read_potatos_by_discord_id():
     # logic for how to find info here
-    pass
+    result_potatoes = []
+    for potato in potatoes:
+        if potato['owner_discord_id'] == discord_id:
+            result_potatoes.append(potato)
+
+    print(result_potatoes)
 
 def update_potato():
 
