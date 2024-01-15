bf29cdb43eed41285c2e3bd7fc5e081de379e188
Fini <enfinity08@gmail.com>
2024-01-15 15:12:10 -0500

    Learning save/load update

--- potato_functions.py
+++ potato_functions.py
@@ -12,11 +12,13 @@ def read_potatos_by_discord_id(a,b):
     return(result_potatoes)
 
 import json
-def loaded_potatoes(saved_filename):
+def load_potatoes(saved_filename):
     with open(save_filename, 'r') as file:
         loaded_potatoes = json.load(file)
 
     return (loaded_potatoes)
+def save_potatoes():
+   pass
 def update_potato():
 
     # logic for how to find info here
