f0b19bb9609f002f36960fe4ae8cba93098bcf50
Fini <enfinity08@gmail.com>
2024-02-02 15:52:45 -0500

    edited lined 5/6 in potato_fn to try and fix gits reqs

--- potato_functions.py
+++ potato_functions.py
@@ -2,8 +2,8 @@ import json
 from pathlib import Path
 import git
 from decouple import config
-database_filename = config("DATABASE_FILEPATH")
-current_directory = config("CURRENT_DIRECTORY")
+database_filename = config("DATABASE_FILEPATH", default="test_potatoes.json")
+current_directory = config("CURRENT_DIRECTORY", default=".")
 
 def create_potato(owner_name, owner_discord_id, name, potato_type, price, accomplishment, date):
         potato = {"owner_name": owner_name,"owner_discord_id": owner_discord_id,"name": name,"potato_type": potato_type,"price": price,"accomplishment": accomplishment,"date": date}
